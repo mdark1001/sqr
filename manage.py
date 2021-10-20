805039838fd5136288ed0aa38ca9169df0c1eeda
mdark1001 <miguel.cabrera.app@gmail.com>
2021-10-20 18:22:16 -0500

    [REF] project structure,[ADD] staticfiles and media server

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoBack.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sqr.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/miguel/proyectos/curso_django/todoBack/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
